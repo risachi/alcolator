fe9dab3b59dda1a7e1c714b3029afbb08a2843e5
risachi <risachi@gmail.com>
2016-03-17 23:57:19 -0700

    checkpoint-20 success

---
+++ alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  alcolator
+//
+//  Created by Lisa on 3/17/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
