0c6bd22ca40a036b08cfa52a55c9e6c317b7064e
risachi <risachi@gmail.com>
2016-03-18 13:49:49 -0700

    checkpoint-21 success

---
+++ alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  alcolator
+//
+//  Created by Lisa on 3/18/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
---
+++ alcolator/MainMenuViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainMenuViewController.m
+//  alcolator
+//
+//  Created by Lisa on 3/18/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
